2b8fec5d9b19a8151457d1b25f7885c736bd3ac6
Michelle Steigerwalt <msteigerwalt@gmail.com>
2018-05-29 15:40:25 -0400

    Rails: config HTTPS -> HTTP for action_mailer.

--- containers/rails/config/environments/development.rb
+++ containers/rails/config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp 
 
   config.action_mailer.default_url_options = {
-    protocol: 'https',
+    protocol: 'http',
     host: 'localhost:8000'
   }
 
